0ba24d651cb8236c3e72a75b417b98d784b9efab
jrines <joshuahrines@gmail.com>
2025-05-13 23:39:26 -0700

    updated sandbox.jl

--- sandbox/sandbox.jl
+++ sandbox/sandbox.jl
@@ -52,5 +52,29 @@ rtb = compute_residuals(ttb_obspts, vtb_ps)
 ## COMPUTE VARIOGRAM FOR ENTIRE SET OF RESIDUALS ##
 ## ============================================= ##
 
+θ = 90*π/180  # angle of anisotropy
 
+# 1) Build two orthogonal direction‐vectors at your target angle θ:
+dir1 =  ( cos(θ),  sin(θ) )   # “major” direction
+dir2 = ( -sin(θ),  cos(θ) )   # ⟂ to dir1
 
+# 2) Compute directional variograms along those:
+g1 = DirectionalVariogram(dir1, rtb, "val",
+                          estimator=:cressie,
+                          maxlag=1000m, dtol=300m)
+γ1 = GeoStatsFunctions.fit(MaternVariogram, g1)
+
+g2 = DirectionalVariogram(dir2, rtb, "val",
+                          estimator=:cressie,
+                          maxlag=1000m, dtol=300m)
+γ2 = GeoStatsFunctions.fit(MaternVariogram, g2)
+
+# 3) Build your anisotropic variogram with those true radii:
+ellipsoid = MetricBall((γ1.ball.radii[1], γ2.ball.radii[1]), Angle2d(θ))
+γ_ani    = MaternVariogram(ellipsoid,
+                           sill   = γ1.sill,
+                           nugget = γ1.nugget)
+
+g_ani = EmpiricalVariogram(rtb, "val", estimator=:cressie, maxlag=1500m, nlags=10)
+# 4) Plot the variogram:
+funplot!(funplot(g_ani), γ_ani, maxlag=1500m, color="red")
